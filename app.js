4e314dbff3ad7df33b3ac31afe4f2a352c20f71f
Marisha-tech <51979512+Marisha-tech@users.noreply.github.com>
2021-03-09 21:59:51 +0700

    Create input

---
+++ app.js
@@ -0,0 +1,17 @@
+const App = {
+  data() {
+    return {
+      placeholderString: 'Введите название заметки',
+      title: 'Список заметок',
+      inputValue: '',
+    }
+  },
+  methods: {
+    inputChangeHandler(event) {
+      // console.log('inputChangeHandler', event.target.value)
+      this.inputValue = event.target.value
+    },
+  },
+}
+
+Vue.createApp(App).mount('#app')
